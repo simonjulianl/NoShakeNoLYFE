08af46177af9696c94e48d055dbc36beea4328c7
simonjulianl <simonjulian21@gmail.com>
2021-01-08 20:29:39 +0800

    add server and game scene complete

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# NoShakeNoLYFE
-Hack N Roll 2021 DeFentilz
+# DeFentilz
+## Requirements
+
+[Node.js](https://nodejs.org) is required to install dependencies and run scripts via `npm`.
+
+## Available Commands
+
+| Command | Description |
+|---------|-------------|
+| `npm install` | Install project dependencies |
+| `npm start` | Build project and open web server running project 
+
+## Writing Code
+After cloning the repo, run `npm install` from your project directory. Then, you can start the local development server by running `node server/index.js`.
+
+## Connecting to Localhost from Other Devices
+1. Make sure that other devices and the main device are in the same LAN
+2. Run `ifconfig | grep "inet " | grep -v 127.0.0.1` on the main device and get the first ip address in the last line 
+3. Connect to `https://<ip_address_found_in_step_1>:8081` from other devices. Note that Generic Sensors API only works under HTTPS connection
+4. If step 3 doesn't work, enable port 8081 in firewall settings of the main device to be used as a screen
+
+## Proposed Features / Changes
+1. Add a start screen
+2. Add support for multiplayer in one screen
+3. Add bosses at certain score level
+4. Increase number of enemies spawned sooner
---
+++ server.js
@@ -0,0 +1,98 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const http = require('https');
+const fs = require('fs');
+
+const server = http.createServer({
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert')
+}, app);
+
+
+const io = require('socket.io')().listen(server);
+const morgan = require('morgan');
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+app.use('/scripts', express.static(path.join(__dirname, 'node_modules')));
+
+app.get('/', (req, res) => {
+    if (req.headers["user-agent"].match(/\bMobile\b/) === null) {
+        res.sendFile(__dirname + '/game.html')
+    } else {
+        res.sendFile(__dirname + '/controller.html')
+    }
+}); // serve index.html
+
+// Setup variables
+const players = {};
+let score = 0;
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on("lobby_scene_connected", () => console.log('lobby scene connected'));
+
+    socket.on('requestPlayers', () => {
+        socket.emit('playerList', players);
+        socket.broadcast.emit('playerList', players);
+    });
+    socket.on('requestScore', () => {
+        socket.emit('currentScore', score);
+        socket.broadcast.emit('currentScore', score);
+    })
+    socket.on('updateScore', (currentScore) => {
+        score = currentScore;
+        socket.emit('currentScore', score);
+        socket.broadcast.emit('currentScore', score);
+    });
+
+    socket.on('playerMovement', (movementInfo) => {
+        socket.broadcast.emit('playerMoved', movementInfo);
+    });
+
+    socket.on('gameLost', () => {
+        socket.emit('openLoseScreen');
+        socket.broadcast.emit('openLoseScreen');
+    })
+
+    socket.on("dropPlayer", (socketId) => {
+        socket.emit("dropPlayer");
+        socket.broadcast.emit("dropPlayer");
+    })
+
+    socket.on('mobileToServer', (socketId) => {
+        // console.log("Mobile Device Connected");
+        // console.log(socketId);
+        socket.emit("newPlayer", socketId)
+        socket.broadcast.emit("newPlayer", socketId);
+    })
+
+    socket.on('moveUp', (socketId) => {
+        socket.broadcast.emit('moveUp', socketId);
+    })
+
+    socket.on('moveDown', (socketId) => {
+        socket.broadcast.emit('moveDown', socketId);
+    })
+
+    socket.on('deviceShaking', (playerId) => {
+        // console.log('Device is Shaking');
+        socket.broadcast.emit("yesShaking", playerId);
+    })
+
+    socket.on('deviceNotShaking', (playerId) => {
+        // console.log('Device is NOT Shaking');
+        socket.broadcast.emit('noShaking', playerId);
+    })
+
+    socket.on('disconnect', () => {
+        console.log("A user disconnected");
+        delete players[socket.id];
+        socket.broadcast.emit('playerList', players);
+        io.emit('disconnected', socket.id);
+    });
+
+    socket.on("window", (window) => console.log(window));
+});
+server.listen(8081, () => console.log(`Listening on ${server.address().port}`));
